97c1f0a1c78b9ff9df30eec1bdf828a2089a0d3a
Tinoe2 <lungalee599@gmail.com>
2025-08-26 11:34:00 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,40 +1,8 @@
-name: Node.js CI
 
-on:
-  push:
-    branches:
-      - main
-  pull_request:
-    branches:
-      - main
-
-jobs:
-  build:
-
-    runs-on: ubuntu-latest
-
-    strategy:
-      matrix:
-        node-version: [20.x]
-
-    steps:
-    - name: Checkout repository
-      uses: actions/checkout@v3
-
-    - name: Set up Node.js
-      uses: actions/setup-node@v3
-      with:
-        node-version: ${{ matrix.node-version }}
-
-    - name: Install dependencies
-      run: npm install
-
-    - name: Start application
-      run: npm start
 module.exports = {
-  SESSION_ID: // "malvin~92EBnKgI#ArvU-pFWV9pNzSrzQXYkQrzZLI-PUysecitKlGp4pDU"
+  SESSION_ID: "malvin~92EBnKgI#ArvU-pFWV9pNzSrzQXYkQrzZLI-PUysecitKlGp4pDU"// 
   
-  OWNER_NUMBER: // "263787856324"
+  OWNER_NUMBER: "263787856324"//
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
