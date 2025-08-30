177f2e73fb120a15736851ac4d37ebb4e3fffd05
Demo <lungalee599@gmail.com>
2025-08-30 06:46:57 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "malvin~92EBnKgI#ArvU-pFWV9pNzSrzQXYkQrzZLI-PUysecitKlGp4pDU"// 
+  SESSION_ID: "malvin~VvdCVTgC#YFA_hE6eQey9OZFeCjOaU7qHArHLe824cZ_8i7Ay8LI"// 
   
   OWNER_NUMBER: "263787856324"//
   
-  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  PREFIX: ".", // prefix (e.g., ?, /, !, *)
   
   TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
 };
